de37cb08a6fe4ecd544cff10cdf9c7596cfdfcf8
peaseloxes <peaseloxes@gmail.com>
2015-09-14 03:35:08 -0500

    Abstract Factory design pattern

---
+++ src/main/java/e20150914/designpatterns/abstractFactory/domain/concrete/AndroidWindow.java
@@ -0,0 +1,15 @@
+package e20150914.designpatterns.abstractFactory.domain.concrete;
+
+import e20150914.designpatterns.abstractFactory.domain.abs.Window;
+
+/**
+ * Created by alex on 9/14/15.
+ */
+public class AndroidWindow extends Window{
+
+    @Override
+    public String toString(){
+        return "Android Window";
+    }
+
+}
---
+++ src/main/java/e20150914/designpatterns/abstractFactory/domain/concrete/WindowsScrollbar.java
@@ -0,0 +1,14 @@
+package e20150914.designpatterns.abstractFactory.domain.concrete;
+
+import e20150914.designpatterns.abstractFactory.domain.abs.Scrollbar;
+
+/**
+ * Created by alex on 9/14/15.
+ */
+public class WindowsScrollbar extends Scrollbar {
+
+    @Override
+    public String toString(){
+        return "Windows Scrollbar";
+    }
+}
